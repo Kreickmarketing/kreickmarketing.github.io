3a83ea373dec364af018fec8ead072fa33b81c40
Kreickmarketing <andrew@kreickmarketing.com>
2026-09-02 22:17:07 -0400

    Delete figma-source directory

--- figma-source/Typography.d.ts
+++
@@ -1,7 +0,0 @@
-import * as React from 'react';
-export interface TypographyProps {
-  className?: string;
-  style?: React.CSSProperties;
-}
-export declare const Typography: React.FC<TypographyProps>;
-export default Typography;
--- figma-source/Variables.d.ts
+++
@@ -1,7 +0,0 @@
-import * as React from 'react';
-export interface VariablesProps {
-  className?: string;
-  style?: React.CSSProperties;
-}
-export declare const Variables: React.FC<VariablesProps>;
-export default Variables;
